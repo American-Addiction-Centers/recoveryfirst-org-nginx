1801cab491836b2b8bcf73ef180fe5539af50949
pbarera1 <phil.barera@gmail.com>
2021-07-19 13:17:06 -0700

    Update app_start.sh

--- hooks/postdeploy/app_start.sh
+++ hooks/postdeploy/app_start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-echo "CLEAR THAT CACHES $WORDPRESS_URL"
-echo "${WORDPRESS_URL/admin/}cache/fullpage/"
-curl --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 40 -X DELETE "https://staging.recoveryfirst.org/cache/fullpage/"
+echo "CLEARING FULL PAGE CACHE"
+curl --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 40 -X DELETE "${WORDPRESS_URL/admin/}cache/fullpage/"
 sleep 2
 # https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/platforms-linux-extend.html
-# $WORDPRESS_URL https://admin.adcare.com/
 # the extra flags on the curl requests are so the command waits for a response
 # https://stackoverflow.com/questions/42873285/curl-retry-mechanism
 # --max-time 10     (how long each retry will wait)
