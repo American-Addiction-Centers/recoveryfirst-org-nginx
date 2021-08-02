46e054572192d945cd0e6525c8ad1f18ee967532
leprekhanh <khanh.hoang@live.com>
2021-08-02 17:15:23 -0500

    Update cache endpoint logic

--- hooks/postdeploy/app_start.sh
+++ hooks/postdeploy/app_start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo "CLEARING FULL PAGE CACHE"
-curl --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 40 -X DELETE "${WORDPRESS_URL/admin/}cache/fullpage/"
+cacheEndpoint=""
+if [ $APP_ENV == "production" ]; then
+	cacheEndpoint="${WORDPRESS_URL/admin./}cache/fullpage/"
+else
+	cacheEndpoint="${WORDPRESS_URL/admin/}cache/fullpage/"
+fi
+curl --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 40 -X DELETE $cacheEndpoint
 sleep 2
 # https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/platforms-linux-extend.html
 # the extra flags on the curl requests are so the command waits for a response
